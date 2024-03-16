84d04698a36bf53fc52bb4c4b1fdcc1f82a9d994
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-03-16 11:33:26 +0500

    Navigation Stack UI

--- src/components/navigationStack/NavigationStack.tsx
+++ src/components/navigationStack/NavigationStack.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable react-native/no-inline-styles */
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
-import { Colors } from '../../constants/Constants';
+import { Colors, Images } from '../../constants/Constants';
+import NavigationPress from '../navigationPress/NavigationPress';
+import HomeIndicator from '../homeIndicator/HomeIndicator';
 
 const NavigationStack = () => {
+  const data = [
+    { iconName: 'HOME_ICON', height: 26, width: 21, title: 'Home' },
+    { iconName: 'REPORTS_ICON', height: 26, width: 23, title: 'Reports' },
+    { iconName: 'PLUS_CIRCLE_ICON', height: 26, width: 18, title: 'Upload' },
+    { iconName: 'PROFILE_ICON', height: 26, width: 18, title: 'Profile' },
+  ];
   return (
     <View
       style={{
         width: '100%',
         height: 62,
         justifyContent: 'center',
-        alignContent: 'center',
+        alignItems: 'center',
       }}
     >
       <View
@@ -22,10 +30,23 @@ const NavigationStack = () => {
           borderRadius: 42,
           borderWidth: 2,
           borderColor: Colors.SECONDARY_COLOR,
+          flexDirection: 'row',
+          gap: 28,
         }}
       >
-        <Text style={{ color: Colors.SECONDARY_COLOR }}>NavigationStack</Text>
+        {data.map((item, index) => {
+          return (
+            <NavigationPress
+              key={index}
+              icon={Images[item.iconName as keyof typeof Images]}
+              title={item.title}
+              height={item.height}
+              width={item.width}
+            />
+          );
+        })}
       </View>
+      <HomeIndicator />
     </View>
   );
 };
