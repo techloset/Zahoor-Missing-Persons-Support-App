ed37605b625659aace8084a399295a0f12d5f116
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-03-16 11:33:37 +0500

    Navigation Press Component UI

--- src/components/navigationPress/NavigationPress.tsx
+++ src/components/navigationPress/NavigationPress.tsx
@@ -1,12 +1,39 @@
+/* eslint-disable react-native/no-inline-styles */
 import { View, Text } from 'react-native';
 import React from 'react';
-import { Images } from '../../constants/Constants';
+import { Colors } from '../../constants/Constants';
+import { FC } from 'react';
+import { SvgProps } from 'react-native-svg';
 
-const NavigationPress = () => {
+interface NavigationPressProps {
+  icon: FC<SvgProps>;
+  title: string;
+  height: number;
+  width: number;
+}
+
+const NavigationPress = ({
+  icon: IconComponent,
+  title,
+  height,
+  width,
+}: NavigationPressProps) => {
   return (
-    <View>
-      <View>{/* <Images. */}</View>
-      <Text>NavigationPress</Text>
+    <View style={{ height: 46, alignItems: 'center' }}>
+      <View style={{ height: height, width: width }}>
+        <IconComponent width={width} height={height} />
+      </View>
+      <Text
+        style={{
+          color: Colors.SECONDARY_COLOR,
+          fontSize: 12,
+          textAlign: 'center',
+          lineHeight: 20,
+          fontWeight: '400',
+        }}
+      >
+        {title}
+      </Text>
     </View>
   );
 };
