09d46313ff4e733aeb766aef092a3aef3af5651c
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-04-05 10:26:06 +0500

    News Screen done

--- src/screens/news/News.tsx
+++ src/screens/news/News.tsx
@@ -1,10 +1,25 @@
-import { ScrollView, Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import { ScrollView } from 'react-native';
 import React from 'react';
+import NewsItem from '../../components/missingPerson/reportListItem/ReportListItem';
+import { Colors, Units } from '../../constants/Constants';
 
 const News = () => {
   return (
-    <ScrollView>
-      <Text>NewsPage</Text>
+    <ScrollView
+      style={{
+        backgroundColor: Colors.WHITE_COLOR,
+        height: Units.WINDOW_HEIGHT,
+        width: Units.WINDOW_WIDTH,
+        paddingTop: 36,
+        paddingHorizontal: 20,
+      }}
+    >
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
+      <NewsItem />
     </ScrollView>
   );
 };
