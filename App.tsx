c252abec0c6d72472a7522a19fd6cb3b539a0b89
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-04-26 09:54:11 +0500

    splash_screen done

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './src/navigation/Navigation';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return <Navigation />;
 };
 
--- android/app/src/main/java/com/findr/MainActivity.kt
+++ android/app/src/main/java/com/findr/MainActivity.kt
@@ -1,21 +1,32 @@
 package com.findr
 
 import com.facebook.react.ReactActivity
+import android.os.Bundle
+import org.devio.rn.splashscreen.SplashScreen
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "Findr"
+    /**
+     * Returns the name of the main component registered from JavaScript. This is used to schedule
+     * rendering of the component.
+     */
+    override fun getMainComponentName(): String = "Findr"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    /**
+     * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
+     * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
+     */
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+    /**
+     * Called when the activity is starting. Add SplashScreen.show(this) here.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this)
+        super.onCreate(savedInstanceState)
+    }
 }
