d4f7fc18837dad7e68817c6d22f3b4cf19612325
ZahoorOnly <i.am.zahoor.a@outlook.com>
2024-04-26 13:15:19 +0500

    NewsItem updated

--- src/components/newsItem/NewsItem.tsx
+++ src/components/newsItem/NewsItem.tsx
@@ -1,31 +1,30 @@
 import { View, Text, Image, Alert } from 'react-native';
 import React from 'react';
 import DetailsButton from '../detailsButton/DetailsButton';
-import { FormData } from '../../types/types';
 import { styles } from './styles';
+import { FormData } from '../../types/types';
 
-type NewsItemProps = {
-  data: FormData;
-  reportedBy: string;
-};
-
-const NewsItem = ({ data, reportedBy }: NewsItemProps) => {
+const NewsItem = ({
+  name,
+  imageUrl,
+  reportDescription,
+  reportLocation,
+  reportedBy,
+}: FormData) => {
   const contactPerson = () => {
     Alert.alert('Contacted Person Details');
   };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={{ uri: data.imageUrl }} style={styles.image} />
+        <Image source={{ uri: imageUrl }} style={styles.image} />
       </View>
       <View style={styles.detailsContainer}>
         <View>
-          <Text style={styles.text}>Name: {data.name}</Text>
+          <Text style={styles.text}>Name: {name}</Text>
           <Text style={styles.text}>Reported by: {reportedBy}</Text>
-          <Text style={styles.text}>Location: {data?.reportLocation}</Text>
-          <Text style={styles.text}>
-            Description: {data?.reportDescription}
-          </Text>
+          <Text style={styles.text}>Location: {reportLocation}</Text>
+          <Text style={styles.text}>Description: {reportDescription}</Text>
         </View>
         <DetailsButton title="Contact Person" onPress={() => contactPerson()} />
       </View>
---
+++ src/components/newsItem/styles.ts
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+import { Colors, Units } from '../../constants/constants';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    minHeight: Units.WINDOW_HEIGHT * 0.1893,
+    width: Units.WINDOW_WIDTH * 0.8907,
+    gap: Units.WINDOW_WIDTH * 0.0213,
+    marginBottom: Units.WINDOW_HEIGHT * 0.0197,
+  },
+  imageContainer: {
+    height: Units.WINDOW_HEIGHT * 0.1893,
+    width: Units.WINDOW_WIDTH * 0.3067,
+    borderRadius: 4,
+    overflow: 'hidden',
+  },
+  image: {
+    height: Units.WINDOW_HEIGHT * 0.1893,
+    width: Units.WINDOW_WIDTH * 0.3067,
+  },
+  detailsContainer: {
+    height: Units.WINDOW_HEIGHT * 0.1622,
+    flex: 1,
+    justifyContent: 'space-between',
+    gap: Units.WINDOW_HEIGHT * 0.0148,
+  },
+  text: {
+    color: Colors.SECONDARY_COLOR,
+    lineHeight: Units.WINDOW_HEIGHT * 0.0296,
+    fontSize: Units.WINDOW_WIDTH * 0.0427,
+    fontWeight: '400',
+  },
+  button: {
+    backgroundColor: Colors.PRIMARY_COLOR,
+    width: Units.WINDOW_WIDTH * 0.248,
+    height: Units.WINDOW_HEIGHT * 0.0296,
+    marginBottom: Units.WINDOW_HEIGHT * 0.0123,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: Colors.WHITE_COLOR,
+  },
+});
